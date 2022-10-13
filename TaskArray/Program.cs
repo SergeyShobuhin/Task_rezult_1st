accfa5b561a8cd8cb2eccfa3c2438c81c42d984a
Sergey Shobuhin <shobuhin@mail.ru>
2022-10-13 19:25:58 +0700

    create array

--- TaskArray/Program.cs
+++ TaskArray/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задаём параметры массива
+Console.WriteLine("Задайте число которое будет равно количеству элементов массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите элементы массива по одному!");
+
+var array = GetArray(); // функция через которую заносим элементы в массив
+PrintArrayToConsole(array); // функция которая показывает исходный массив
+
+
+string[] GetArray()
+{
+    var array = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите элемент {0}", i + 1);
+        array[i] = String.Format(Console.ReadLine());
+    }
+
+    return array;
+}
+
+void PrintArrayToConsole(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        // Console.WriteLine(array[i]);        
+    }
+    Console.WriteLine($"Ваш массив [{string.Join(",", array)}] ");
+}
+
+
